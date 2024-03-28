3e85c00d87a466629528e1f0fe012553870cc926
Aswini2414 <tejaswini2414154@gmail.com>
2024-03-28 21:06:45 +0530

    updated

--- routes/route.js
+++ routes/route.js
@@ -8,7 +8,7 @@ const router = express.Router();
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, "files");
+        cb(null, "../files");
     },
     filename: function (req, file, cb) {
         const date = Date.now();
@@ -39,7 +39,7 @@ router.post("/generate-pdf", async (req, res) => {
     console.log("hi");
     try {
         const { filePath, selectedPages, pdfFile } = req.body;
-        const ExistingPdfBytes = await fs.readFile(`../files/${pdfFile}`);
+        const ExistingPdfBytes = await fs.readFile(filePath);
         const ExistingPdfDoc = await PDFDocument.load(ExistingPdfBytes);
         const newPdfDoc = await PDFDocument.create();
         for (const i of selectedPages) {
@@ -47,7 +47,7 @@ router.post("/generate-pdf", async (req, res) => {
             newPdfDoc.addPage(copiedPage);
         }
         const newPdfBytes = await newPdfDoc.save();
-        await fs.writeFile(`../Generated_pdf/generated${pdfFile}`, newPdfBytes);
+        await fs.writeFile(`./Generated_pdf/generated${pdfFile}`, newPdfBytes);
         const newPdf = await Pdf.create({ pdf: `generated${pdfFile}` });
         await newPdf.save();
         res.status(201).json(newPdf);
