ebc1f7e48ef5c2ec5c85de514b6d1b5ef6b0b45b
Aswini2414 <tejaswini2414154@gmail.com>
2024-03-28 23:34:42 +0530

    updated

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const express = require("express");
 const connection = require("./database/db");
 const cors = require('cors');
 const routes = require("./routes/route.js");
+const path = require("path");
+
 
 const app = express();
+const __dirname1 = path.resolve();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.use("/files", express.static("files"));
-app.use("/generated", express.static("Generated_pdf"));
+app.use("/files", express.static(path.join(__dirname1,"./files")));
+app.use("/generated", express.static(path.join(__dirname1,"./Generated_pdf"));
 
 const port = 5000;
 
